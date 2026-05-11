351bb862b43e593f39a8772806ffda76a00fb70f
Brian Dickson <contactbriandickson@gmail.com>
2026-05-11 16:04:36 +0100

    Add articles section with first article

---
+++ articles.md
@@ -0,0 +1,13 @@
+---
+layout: base.njk
+title: Articles
+---
+
+# Articles
+
+{% for post in collections.articles | reverse %}
+  <article class="post-item">
+    <h2><a href="{{ post.url }}">{{ post.data.title }}</a></h2>
+    <p class="date">{{ post.date | postDate }}</p>
+  </article>
+{% endfor %}
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,14 @@
 export default function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
+
+    eleventyConfig.addFilter("postDate", (date) => {
+    return new Date(date).toLocaleDateString("en-GB", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    });
+  });
+  
   return {
     dir: {
       input: ".",
